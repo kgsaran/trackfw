f172582bedf44731db2cba589a361fdbe9a83523
Kleber Saran <kg.saran@gmail.com>
2026-07-27 21:17:40 -0300

    chore(release): bump version files to 3.1.0 (#83)
    Co-authored-by: Kleber Gustavo Saran <kgsaran@gmail.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-var Version = "v3.0.0"
+var Version = "v3.1.0"
--- pypi/trackfw/__init__.py
+++ pypi/trackfw/__init__.py
@@ -1,5 +1,5 @@
 try:
     from importlib.metadata import version
-    __version__ = version("trackfw") or "3.0.0"
+    __version__ = version("trackfw") or "3.1.0"
 except Exception:
-    __version__ = "3.0.0"
+    __version__ = "3.1.0"
